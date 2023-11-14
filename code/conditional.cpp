6d1e74e804cfc936df241aebf5f6a2b3535e14b6
Dipokal HHJ <hhj@devent.kr>
2023-11-14 15:48:02 +0900

    docs: add table of contents

---
+++ code/conditional.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int checkNumber = 20;
+    if (checkNumber > 30) {
+        cout << "30보다 작은데요.. 근데 왜 기준이 30이지.." << endl;
+    } else {
+        cout << "30보다 큰데요.. 근데 왜 기준이 30이지.." << endl;
+    }
+    // 왜 다른 언어랑 별 차이가 없는거지. 
+    
+    return 0;
+}
