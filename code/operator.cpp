cb2ba4b63ef627fa85c29f5270bc65e45ccb469d
Dipokal HHJ <hhj@devent.kr>
2023-11-14 10:10:33 +0900

    feat: add type

---
+++ code/operator.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 80;
+    int b = 24;
+    cout << "+ is " << a + b << endl;
+    cout << "- is " << a - b << endl;
+    cout << "* is " << a * b << endl;
+    cout << "/ is " << a / b << endl;
+
+    return 0;
+}
